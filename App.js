e71ff1dd9659933fc7d2c66b4a5fd5018326e06f
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2021-11-04 08:03:42 -0300

    insere parametros dimensionais do app

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>CAMPO MINADO</Text>
+        <Text>Tamanho da grade:
+            {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>CAMPO MINADO</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
